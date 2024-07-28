b5da93a2097003e3c91b24fe11c3ce74a937e40e
IliaGlaz <glazterix@gmail.com>
2024-07-28 16:12:44 +0300

    BinarySearch

---
+++ src/main/kotlin/BinarySearch.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val list = listOf(1, 4, 20, 205, 206, 207, 500)
+
+    println(binarySearch(list, 20))
+}
+
+fun <A : Comparable<A>> binarySearch(list: List<A>, target: A): Int {
+    var start = 0
+    var last = list.lastIndex
+
+    while (start <= last) {
+        var mid = (last + start) / 2
+        if (list[mid] == target) {
+            return mid
+        }
+        if (list[mid] < target) {
+            start = mid + 1
+        }
+        if (list[mid] > target) {
+            last = mid - 1
+        }
+    }
+
+    return -1
+}
