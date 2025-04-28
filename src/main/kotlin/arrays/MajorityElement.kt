42497757540f82293ff6a6f510463406a681fbd6
IlyaGlaz <glazterix@gmail.com>
2025-04-28 14:31:52 +0300

    MajorityElement

--- src/main/kotlin/arrays/MajorityElement.kt
+++ src/main/kotlin/arrays/MajorityElement.kt
@@ -1,17 +1,49 @@
 package arrays
 
+import java.lang.Integer.max
+
 fun majorityElement(nums: IntArray): Int {
-    val memo = mutableMapOf<Int, Int>()
+    val count = mutableMapOf<Int, Int>()
+    var res = 0
+    var maxCount = 0
 
-    for (i in nums.indices) {
-        val num = nums[i]
-
-        memo[num] = memo.getOrPut(num) { 1 } + 1
+    for (num in nums) {
+        count[num] = count.getOrPut(num) { 1 } + 1
+        if (count[num]!! > maxCount) {
+            res = num
+        }
+        maxCount = max(maxCount, count[num]!!)
     }
 
-    return memo.maxBy { it.value }.key
+    return res
+}
+
+fun majorityElementIdiomatic(nums: IntArray): Int {
+    val counts = mutableMapOf<Int, Int>()
+    var res = 0
+
+    nums.forEach { counts[it] = counts.getOrDefault(it, 0) + 1 }
+    counts.forEach { if (it.value > nums.size / 2) res = it.key }
+
+    return res
+}
+
+fun majorityElementNoMemory(nums: IntArray): Int {
+    var res = 0
+    var count = 0
+
+    for (num in nums) {
+        if (count == 0) {
+            res = num
+        }
+
+        if (res == num) count++
+        else count--
+    }
+
+    return res
 }
 
 fun main() {
-    println(majorityElement(intArrayOf(2, 2, 3, 3, 2, 4)))
+    println(majorityElementNoMemory(intArrayOf(2, 2, 3, 3, 2, 3, 3, 4)))
 }
